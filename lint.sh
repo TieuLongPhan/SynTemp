dbac8523c77d5469db4b88420f8296c5c736c31b
Tieu Long Phan <ptlong.chcndpbc20@ump.edu.vn>
2024-05-29 10:16:02 +0200

    update

--- lint.sh
+++ lint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-flake8 . --count --max-complexity=13 --max-line-length=90 \
+flake8 . --count --max-complexity=13 --max-line-length=100 \
 	--per-file-ignores="__init__.py:F401" \
 	--per-file-ignores="rule_engine.py:F403" \
 	--statistics
